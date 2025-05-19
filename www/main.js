0c0941aac99b22a1b211b2afdee5a95682345cc0
Jaykishan <jaykishanlokhande1013@gmaiil.com>
2025-05-19 12:40:01 +0530

    video-5

--- www/main.js
+++ www/main.js
@@ -1,4 +1,5 @@
 $(document).ready(function () {
+    // Text animation
     $('.text').textillate({
         loop: true,
         sync: true,
@@ -8,18 +9,7 @@ $(document).ready(function () {
         out: {
             effect: "bounceOut",
         }
-    }); 
-});
-// Siri configuration
-    var siriWave = new SiriWave({
-        container: document.getElementById("siri-container"),
-        width: 800,
-        height: 200,
-        style: "ios9",
-        amplitude: "1",
-        speed: "0.30",
-        autostart: true
-      });
+    });
 
     // Siri message animation
     $('.siri-message').textillate({
@@ -32,6 +22,25 @@ $(document).ready(function () {
         out: {
             effect: "fadeOutUp",
             sync: true,
-        },
-
+        }
     });
+
+    // Mic button click event
+    $("#MicBtn").click(function () {
+        eel.playAssistantSound();
+        $("#Oval").attr("hidden", true);
+        $("#SiriWave").attr("hidden", false);
+        eel.allCommands()();
+    });
+
+    // SiriWave configuration
+    var siriWave = new SiriWave({
+        container: document.getElementById("siri-container"),
+        width: 800,
+        height: 200,
+        style: "ios9",
+        amplitude: 1, // changed from string to number
+        speed: 0.30,  // changed from string to number
+        autostart: true
+    });
+});
