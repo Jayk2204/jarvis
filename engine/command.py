397e2e2d5ff3c9c668ba33c69bc45a73fa47aa4e
Jaykishan <jaykishanlokhande1013@gmaiil.com>
2025-05-19 20:54:28 +0530

    7- video

--- engine/command.py
+++ engine/command.py
@@ -1,4 +1,5 @@
 import pyttsx3
+import speech_recognition as sr
 
 
 def speak(text):
@@ -10,5 +11,28 @@ def speak(text):
     engine.say(text)
     engine.runAndWait()
     
-speak("hello jaykishan")
+def takecommand():
+    r = sr.Recognizer()
+    
+    with sr.Microphone() as source:
+        print('Listening....')
+        r.pause_threshold=1
+        r.adjust_for_ambient_noise(source)
+        
+        audio=r.listen(source,10,6)
+        
+    try:
+        print('Recognizing')
+        query=r.recognize_google(audio,language='en-in')
+        print(f"user said: {query}")
+    except Exception as e:
+        return ""
+    
+    return query.lower()
+
+text= takecommand()
+
+        
+
+speak(text)
     
