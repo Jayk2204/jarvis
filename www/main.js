c93e0bd1e1f369630487d8b7253daf16b47e956a
Jaykishan <jaykishanlokhande1013@gmaiil.com>
2025-05-19 21:36:41 +0530

    video-8

--- www/main.js
+++ www/main.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
         eel.playAssistantSound();
         $("#Oval").attr("hidden", true);
         $("#SiriWave").attr("hidden", false);
-        eel.allCommands()();
+        eel.takecommand()();
     });
 
     // SiriWave configuration
