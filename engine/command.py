5c3d239fcbedc4d8fc489e9c4276c43ca52cc2af
Jaykishan <jaykishanlokhande1013@gmaiil.com>
2025-05-20 10:11:42 +0530

    video-9

--- engine/command.py
+++ engine/command.py
@@ -9,7 +9,7 @@ def speak(text):
     voices = engine.getProperty('voices')
     engine.setProperty('voice', voices[0].id)
     engine.setProperty('rate', 174)
-    print(voices)
+    eel.DisplayMessage(text)
     engine.say(text)
     engine.runAndWait()
 
@@ -30,10 +30,29 @@ def takecommand():
             query = r.recognize_google(audio, language='en-in')
             print(f"User said: {query}")
             eel.DisplayMessage(query)
-            speak(query)
-            eel.ShowHood()
+            
+            
+            return query
         
         except Exception as e:
             print(f"Error: {e}")
             eel.DisplayMessage("Sorry, I couldn't hear you.")
             return ""
+
+
+@eel.expose
+def allCommands():
+    query = takecommand()
+    print(query)
+    
+    if query and "open" in query:
+        from engine.features import openCommand
+        openCommand(query)
+        
+    elif "on youtube":
+        from engine.features import PlayYoutube
+        PlayYoutube(query)
+    else:
+        print("not run")
+    
+    eel.ShowHood()
--- www/main.js
+++ www/main.js
@@ -30,7 +30,7 @@ $(document).ready(function () {
         eel.playAssistantSound();
         $("#Oval").attr("hidden", true);
         $("#SiriWave").attr("hidden", false);
-        eel.takecommand()();
+        eel.allCommands()();
     });
 
     // SiriWave configuration
